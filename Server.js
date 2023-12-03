9e9852713d50f54ab8db01646ba7049e292429c2
LauraTafaj <121570865+LauraTafaj@users.noreply.github.com>
2023-12-03 17:12:44 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,5 +1,5 @@
-var PORT = 33333;
-var HOST = '';
+var PORT = 33331;
+var HOST = '0.0.0.0';
 const fs = require ('fs');
 var path = 'mesazhi.txt';
 errorcode = "NUK KENI QASJE!"
@@ -14,15 +14,3 @@ server.on('listening', function() {
   var address = server.address();
  console.log(`Serveri udp eshte aktiv ne adresen:  ${address.address}:${address.port}`);
 });
-server.on('message', function(message, remote) {
- console.log(remote.address + ':' + remote.port +' - ' + message);
- const reply = 'Mesazhi ka arritur me sukses ne server!';
- server.send(reply, 0, reply.length, remote.port, remote.address, function(err, bytes) {
-  if (err) throw err;
-  console.log(
-    `Pergjigja e serverit u dergua tek klienti ${remote.address}:${remote.port}`
-  );
-  });
-});
-
-server.bind(PORT, HOST);
