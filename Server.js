19c72abf3b4aa93238b962b9e6eb7ed4f03ec50c
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2023-12-04 06:15:17 +1400

    Update Server.js

--- Server.js
+++ Server.js
@@ -14,3 +14,15 @@ server.on('listening', function() {
   var address = server.address();
  console.log(`Serveri udp eshte aktiv ne adresen:  ${address.address}:${address.port}`);
 });
+server.on('message', function(message, remote) {
+ console.log(remote.address + ':' + remote.port +' - ' + message);
+ const reply = 'Mesazhi ka arritur me sukses ne server!';
+ server.send(reply, 0, reply.length, remote.port, remote.address, function(err, bytes) {
+  if (err) throw err;
+  console.log(
+    `Pergjigja e serverit u dergua tek klienti ${remote.address}:${remote.port}`
+  );
+  });
+});
+
+server.bind(PORT, HOST);
